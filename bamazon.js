8c7a817c17066683b36fdeddeec9e16e75fc3467
Matt Colson <mc180g@gmail.com>
2020-03-01 17:43:57 -0500

    cleaned syntax

--- bamazon.js
+++ bamazon.js
@@ -16,81 +16,79 @@ var connection = mysql.createConnection({
   database: "bamazonDB"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
   queryAllProducts();
 });
 
 function queryAllProducts() {
-    connection.query("SELECT * FROM products", function(err, res) {
-      if (err) throw err;
-      console.log("Hello Traveler! Let me show you my wares...");
-      console.log("------------------------------------");
-      for (var i = 0; i < res.length; i++) {
-        console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].price);
-      }
-      console.log("-----------------------------------");
-    });
-  }
+  connection.query("SELECT * FROM products", function (err, res) {
+    if (err) throw err;
+    console.log("Hello Traveler! Let me show you my wares...");
+    console.log("------------------------------------");
+    for (var i = 0; i < res.length; i++) {
+      console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].price);
+    }
+    console.log("-----------------------------------");
+  });
+}
 
-  
+
 function userQuery() {
   inquire.prompt(
-      {
-  name: "shop",
-  type: "number",
-  message: "What product would you like to buy? (Please enter id#)",
-      })
-      .then(function (answer) {
+    {
+      name: "shop",
+      type: "number",
+      message: "What product would you like to buy? (Please enter id#)",
+    })
+    .then(function (answer) {
       var query = "SELECT product_name, price FROM products WHERE ?";
-        connection.query( query, {id: id.answer}, function(err, res) {
-    if (err) throw err;    
-    for (let i = 0; i < res.length;i++)
-    console.log(res[i].item_id + " | " + res[i].product_name + " | $" + res[i].price);
-        });
-})};
+      connection.query(query, { id: id.answer }, function (err, res) {
+        if (err) throw err;
+        for (let i = 0; i < res.length; i++)
+          console.log(res[i].item_id + " | " + res[i].product_name + " | $" + res[i].price);
+      });
+    })
+};
 
-itemQuantity()    {
+function itemQuantity() {
   inquire.prompt(
     {
       name: "quantity",
-      type:"number",
-      message:"And how many can I put you down for? Please select from 1 to " + res[i].quantity + ".",
+      type: "number",
+      message: "And how many can I put you down for? Please select from 1 to " + res[i].quantity + ".",
     })
-      .then(function (howMany){
-        if (quantity < res[i].quantity) throw err;
-        console.log("I just don't have enough right now. Perhaps you can buy " + res[i].quantity -1 + "?");
-        else ()
-      })
-       
-         
-    };
-//     {
-// name: "continue",
-// type: "list",
-// message:"Would you like to checkout?",
-// choices: ["Yes", "No", "Cancel"],
-// checkOut();
-//       }
-// )
+    .then(function (howMany) {
+      if (quantity < res[i].quantity) throw err;
+      console.log("I just don't have enough right now. Perhaps you can buy " + res[i].quantity - 1 + "?");
+
+    })
 
 
- 
+};
+function checkOut() {
+  inquire.prompt(
+    {
+      name: "continue",
+      type: "list",
+      message: "Would you like to checkout?",
+      choices: ["Yes", "No", "Cancel"],
+    })
 
-//Take User Quanity and multiply by Price for the Total.
-  // function checkOut() {
-  //     userQuanity = value;
-  //     (price * userQuanity = userTotal)
 
-      //Extra Credit: Add 5% sales tax.
-  }
 
-// console.log("It seems you would like to purchase " + userQuantity + " " + product_name + "'s at $" + price + ", which would bring your total to $" + userTotal + ". What card would you like to use?");
+
+  // Take User Quanity and multiply by Price for the Total.
+    function checkOut() {
+      userTotal = price * userQuanity;
+
+        // Extra Credit: Add 5% sales tax.
+    }
+
+  console.log("It seems you would like to purchase " + userQuantity + " " + product_name + "'s at $" + price + ", which would bring your total to $" 
+  + userTotal + ". What card would you like to use?");
 
   // logs the actual query being run
   console.log(query.sql);
   connection.end();
-
- 
-}
